8fe947f546c444e4ca59f3fc537b42b9e4bbc47e
Nehakamble-217 <kambleneha217@gmail.com>
2024-06-04 19:03:05 +0530

    add functionality to candidate form

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,8 @@ import axios from "axios";
 const baseURL = "http://192.168.1.40:8891/api/ats/157industries";
 
 export const getPasswordFromDB = (id) =>
-  axios.post(`${baseURL}/fetch-pass/${id}`);
+  axios.post(`${baseURL}/fe
+tch-pass/${id}`);
 
   export const getEmployeeWorkData = (id) =>
 axios.get(`${baseURL}/employee-work/${id}`);
