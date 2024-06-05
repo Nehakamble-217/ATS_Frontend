228270900cc1af468daa67c7fe614939baa601f5
Azhar <tamboliazhar602@gmail.com>
2024-06-05 19:20:58 +0530

    Ajhar 5-6-24 7:15

--- src/CandidateSection/ShortListedCandidate.jsx
+++ src/CandidateSection/ShortListedCandidate.jsx
@@ -15,7 +15,7 @@ const ShortListedCandidates = () => {
     const fetchShortListedData = async () => {
         try {
             const response = await fetch(
-                `http://192.168.1.40:8891/api/ats/157industries/shortListed-date/${newEmployeeId}`
+                `http://192.168.1.41:8891/api/ats/157industries/shortListed-date/${newEmployeeId}`
             );
             const data = await response.json();
             setShortListedData(data);
