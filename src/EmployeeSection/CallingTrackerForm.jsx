aa1725b8ce73099b011bdb86b7a839196004d9bb
Azhar <tamboliazhar602@gmail.com>
2024-06-14 13:27:51 +0530

    Ajhar 14-6

--- src/EmployeeSection/CallingTrackerForm.jsx
+++ src/EmployeeSection/CallingTrackerForm.jsx
@@ -37,6 +37,7 @@ const CallingTrackerForm = ({
     callingFeedback: "",
     totalExperienceYears: '',
     totalExperienceMonths: '',
+    relaventExperience: '',
     currentsalarylakh: '',
     currentsalaryth: '',
     employee: {
@@ -440,7 +441,7 @@ const CallingTrackerForm = ({
               </div>
 
               <tr id="heading123">
-                <th> Candidates Full Name*</th>
+                <th> Candidates Full Name* </th>
                 <td>
                   <input
                     type="text"
@@ -462,6 +463,7 @@ const CallingTrackerForm = ({
 
 
                   className={`form-control ${error ? 'is-invalid' : ''}`}
+                  required
                 />
                 
               </td>
@@ -479,6 +481,7 @@ const CallingTrackerForm = ({
                     maxLength={11}
                     className="PhoneInputInput"
                     name="contactNumber"
+                    required
                   />
                 </td>
 
@@ -585,6 +588,7 @@ const CallingTrackerForm = ({
                 <option value="Pune City">Pune City</option>
                 <option value="PCMC">PCMC</option>
                 <option value="Other">Other</option>
+                
               </select>
             ) : (
               <input
@@ -594,6 +598,7 @@ const CallingTrackerForm = ({
                 onChange={handleLocationInputChange}
                 className="form-control"
                 placeholder="Enter your location"
+                required
               />
             )}
           </td> 
@@ -669,6 +674,7 @@ const CallingTrackerForm = ({
               value={lineUpData.dateOfBirth}
               onChange={handleDateChange}
               className="form-control"
+              required
             />
           </td>
 
@@ -728,6 +734,7 @@ const CallingTrackerForm = ({
                 className="form-control"
               >
                 <option value="">__Select __</option>
+                <option value="Other">Other</option>
                 <option value="10th">10th</option>
                 <option value="12th">12 th</option>
                 <option value="ITI">ITI</option>
@@ -739,7 +746,136 @@ const CallingTrackerForm = ({
                 <option value="MSC">MSC </option>
                 <option value="BCA">BCA</option>
                 <option value="MCA">MCA</option>
-                <option value="ITI">Other</option>
+                <option value= "Associate of Arts (AA)">Associate of Arts (AA)</option>
+<option value ="Associate of Science (AS)">Associate of Science (AS)</option>
+<option value= "Associate of Applied Science (AAS)">Associate of Applied Science (AAS)</option>
+<option value="Associate of Fine Arts (AFA)">Associate of Fine Arts (AFA)</option>
+<option value="Associate of Business Administration (ABA)">Associate of Business Administration (ABA)</option>
+<option value="Associate of Engineering (AE)">Associate of Engineering (AE)</option>
+<option value="Associate of Nursing (AN)">Associate of Nursing (AN)</option>
+<option value="Associate of General Studies (AGS)">Associate of General Studies (AGS)</option>
+<option value="Associate of Occupational Studies (AOS)">Associate of Occupational Studies (AOS)</option>
+<option value="Associate of Information Technology (AIT)">Associate of Information Technology (AIT)</option>
+<option value="Bachelor's Degrees">Bachelor's Degrees</option>
+<option value="Bachelor of Arts (BA)">Bachelor of Arts (BA)</option>
+<option value="Bachelor of Science (BS)">Bachelor of Science (BS)</option>
+<option value="Bachelor of Fine Arts (BFA)">Bachelor of Fine Arts (BFA)</option>
+<option value="Bachelor of Business Administration (BBA)">Bachelor of Business Administration (BBA)</option>
+<option value="Bachelor of Engineering (BEng)">Bachelor of Engineering (BEng)</option>
+<option value="Bachelor of Technology (BTech)">Bachelor of Technology (BTech)</option>
+<option value="Bachelor of Education (BEd)">Bachelor of Education (BEd)</option>
+<option value="Bachelor of Nursing (BN)">Bachelor of Nursing (BN)</option>
+<option value="Bachelor of Social Work (BSW)">Bachelor of Social Work (BSW)</option>
+<option value="Bachelor of Music (BM)">Bachelor of Music (BM)</option>
+<option value="Bachelor of Architecture (BArch)">Bachelor of Architecture (BArch)</option>
+<option value="Bachelor of Science in Nursing (BSN)">Bachelor of Science in Nursing (BSN)</option>
+<option value="Bachelor of Computer Science (BCS)">Bachelor of Computer Science (BCS)</option>
+<option value="Bachelor of Laws (LLB)">Bachelor of Laws (LLB)</option>
+<option value="Bachelor of Medicine, Bachelor of Surgery (MBBS)">Bachelor of Medicine, Bachelor of Surgery (MBBS)</option>
+<option value="Bachelor of Dental Surgery (BDS)">Bachelor of Dental Surgery (BDS)</option>
+<option value="Bachelor of Pharmacy (BPharm)">Bachelor of Pharmacy (BPharm)</option>
+<option value="Bachelor of Public Health (BPH)">Bachelor of Public Health (BPH)</option>
+<option value="Bachelor of Environmental Science (BES)">Bachelor of Environmental Science (BES)</option>
+<option value="Bachelor of Communication (BComm)">Bachelor of Communication (BComm)</option>
+<option value="Bachelor of Information Technology (BIT)">Bachelor of Information Technology (BIT)</option>
+<option value="Bachelor of Science in Engineering (BSE)">Bachelor of Science in Engineering (BSE)</option>
+<option value="Bachelor of Business (BBus)">Bachelor of Business (BBus)</option>
+<option value="Bachelor of Design (BDes)">Bachelor of Design (BDes)</option>
+<option value="Bachelor of Journalism (BJ)">Bachelor of Journalism (BJ)</option>
+<option value="Bachelor of Applied Science (BAS)">Bachelor of Applied Science (BAS)</option>
+<option value="Bachelor of Agriculture (BAgri)">Bachelor of Agriculture (BAgri)</option>
+<option value="Bachelor of Veterinary Science (BVSc)">Bachelor of Veterinary Science (BVSc)</option>
+<option value="Bachelor of Physiotherapy (BPT)">Bachelor of Physiotherapy (BPT)</option>
+<option value="Master's Degrees">Master's Degrees</option>
+<option value="Master of Arts (MA)">Master of Arts (MA)</option>
+<option value="Master of Science (MS or MSc)">Master of Science (MS or MSc)</option>
+<option value="Master of Business Administration (MBA)">Master of Business Administration (MBA)</option>
+<option value="Master of Fine Arts (MFA)">Master of Fine Arts (MFA)</option>
+<option value="Master of Education (MEd)">Master of Education (MEd)</option>
+<option value="Master of Engineering (MEng)">Master of Engineering (MEng)</option>
+<option value="Master of Technology (MTech)">Master of Technology (MTech)</option>
+<option value="Master of Social Work (MSW)">Master of Social Work (MSW)</option>
+<option value="Master of Music (MM)">Master of Music (MM)</option>
+<option value="Master of Architecture (MArch)">Master of Architecture (MArch)</option>
+<option value="Master of Public Health (MPH)">Master of Public Health (MPH)</option>
+<option value="Master of Laws (LLM)">Master of Laws (LLM)</option>
+<option value="Master of Computer Applications (MCA)">Master of Computer Applications (MCA)</option>
+<option value="Master of Science in Nursing (MSN)">Master of Science in Nursing (MSN)</option>
+<option value="Master of Library Science (MLS)">Master of Library Science (MLS)</option>
+<option value="Master of Public Administration (MPA)">Master of Public Administration (MPA)</option>
+<option value="Master of Philosophy (MPhil)">Master of Philosophy (MPhil)</option>
+<option value="Master of Professional Studies (MPS)">Master of Professional Studies (MPS)</option>
+<option value="Master of Design (MDes)">Master of Design (MDes)</option>
+<option value="Master of Journalism (MJ)">Master of Journalism (MJ)</option>
+<option value="Master of Environmental Science (MES)">Master of Environmental Science (MES)</option>
+<option value="Master of Communication (MComm)">Master of Communication (MComm)</option>
+<option value="Master of International Business (MIB)">Master of International Business (MIB)</option>
+<option value="Master of Finance (MFin)">Master of Finance (MFin)</option>
+<option value="Master of Management (MMgt)">Master of Management (MMgt)</option>
+<option value="Master of Science in Engineering (MSE)">Master of Science in Engineering (MSE)</option>
+<option value="Master of Health Administration (MHA)">Master of Health Administration (MHA)</option>
+<option value="Master of Urban Planning (MUP)">Master of Urban Planning (MUP)</option>
+<option value="Master of Data Science (MDS)">Master of Data Science (MDS)</option>
+<option value="Doctoral Degrees">Doctoral Degrees</option>
+<option value="Doctor of Philosophy (PhD)">Doctor of Philosophy (PhD)</option>
+<option value="Doctor of Medicine (MD)">Doctor of Medicine (MD)</option>
+<option value="Doctor of Education (EdD)">Doctor of Education (EdD)</option>
+<option value="Doctor of Business Administration (DBA)">Doctor of Business Administration (DBA)</option>
+<option value="Doctor of Dental Surgery (DDS)">Doctor of Dental Surgery (DDS)</option>
+<option value="Doctor of Dental Medicine (DMD)">Doctor of Dental Medicine (DMD)</option>
+<option value="Doctor of Veterinary Medicine (DVM)">Doctor of Veterinary Medicine (DVM)</option>
+<option value="Doctor of Nursing Practice (DNP)">Doctor of Nursing Practice (DNP)</option>
+<option value="Doctor of Psychology (PsyD)">Doctor of Psychology (PsyD)</option>
+<option value="Juris Doctor (JD)">Juris Doctor (JD)</option>
+<option value="Doctor of Public Health (DrPH)">Doctor of Public Health (DrPH)</option>
+<option value="Doctor of Pharmacy (PharmD)">Doctor of Pharmacy (PharmD)</option>
+<option value="Doctor of Physical Therapy (DPT)">Doctor of Physical Therapy (DPT)</option>
+<option value="Doctor of Engineering (DEng or DScEng)">Doctor of Engineering (DEng or DScEng)</option>
+<option value="Doctor of Science (DSc)">Doctor of Science (DSc)</option>
+<option value="Doctor of Musical Arts (DMA)">Doctor of Musical Arts (DMA)</option>
+<option value="Doctor of Social Work (DSW)">Doctor of Social Work (DSW)</option>
+<option value="Doctor of Information Technology (DIT)">Doctor of Information Technology (DIT)</option>
+<option value="Doctor of Health Science (DHSc)">Doctor of Health Science (DHSc)</option>
+<option value="Doctor of Public Administration (DPA)">Doctor of Public Administration (DPA)</option>
+<option value="Diplomas and Certificates">Diplomas and Certificates</option>
+<option value="Diploma in Engineering">Diploma in Engineering</option>
+<option value="Diploma in Nursing">Diploma in Nursing</option>
+<option value="Diploma in Education">Diploma in Education</option>
+<option value="Diploma in Business Studies">Diploma in Business Studies</option>
+<option value="Diploma in Computer Applications">Diploma in Computer Applications</option>
+<option value="Diploma in Culinary Arts">Diploma in Culinary Arts</option>
+<option value="Diploma in Graphic Design">Diploma in Graphic Design</option>
+<option value="Diploma in Information Technology">Diploma in Information Technology</option>
+<option value="Diploma in Pharmacy">Diploma in Pharmacy</option>
+<option value="Diploma in Accounting">Diploma in Accounting</option>
+<option value="Diploma in Marketing">Diploma in Marketing</option>
+<option value="Diploma in Hospitality Management">Diploma in Hospitality Management</option>
+<option value="Diploma in Fashion Design">Diploma in Fashion Design</option>
+<option value="Diploma in Project Management">Diploma in Project Management</option>
+<option value="Diploma in Electrical Engineering">Diploma in Electrical Engineering</option>
+<option value="Diploma in Mechanical Engineering">Diploma in Mechanical Engineering</option>
+<option value="Diploma in Civil Engineering">Diploma in Civil Engineering</option>
+<option value="Diploma in Health Sciences">Diploma in Health Sciences</option>
+<option value="Diploma in Environmental Science">Diploma in Environmental Science</option>
+<option value="Diploma in Journalism">Diploma in Journalism</option>
+<option value="Diploma in Social Work">Diploma in Social Work</option>
+<option value="Diploma in Early Childhood Education">Diploma in Early Childhood Education</option>
+<option value="Diploma in Interior Design">Diploma in Interior Design</option>
+<option value="Diploma in Event Management">Diploma in Event Management</option>
+<option value="Diploma in Human Resource Management">Diploma in Human Resource Management</option>
+<option value="Diploma in Digital Marketing">Diploma in Digital Marketing</option>
+<option value="Diploma in Financial Management">Diploma in Financial Management</option>
+<option value="Diploma in Logistics and Supply Chain Management">Diploma in Logistics and Supply Chain Management</option>
+<option value="Diploma in Biotechnology">Diploma in Biotechnology</option>
+<option value="Diploma in Tourism Management">Diploma in Tourism Management</option>
+<option value="Diploma in Public Relations">Diploma in Public Relations</option>
+<option value="Diploma in Web Development">Diploma in Web Development</option>
+<option value="Diploma in Film and Television Production">Diploma in Film and Television Production</option>
+<option value="Diploma in Software Engineering">Diploma in Software Engineering</option>
+<option value="Diploma in Agriculture">Diploma in Agriculture</option>
+<option value="Diploma in Cybersecurity">Diploma in Cybersecurity</option>
+<option value="Diploma in Data Science">Diploma in Data Science</option>
+<option value="Diploma in Artificial Intelligence">Diploma in Artificial Intelligence</option>
                
               </select>
             ) : (
@@ -749,7 +885,7 @@ const CallingTrackerForm = ({
                 value={callingTracker.currentEducation}
                 onChange={handleeducationInputChange}
                 className="form-control"
-                placeholder="Enter your location"
+                placeholder="Enter your Education"
               />
             )}
           </td> 
@@ -872,14 +1008,15 @@ const CallingTrackerForm = ({
               </tr>
               <tr>
 
-              <th scope="col">Relavent Exp</th>
+              <th scope="col">Relavent Experince</th>
                 <td>
                 <input
-                  type="email"
-                  name="relaventexp"
-                  value=""
-                  
-                  className={`form-control ${error ? 'is-invalid' : ''}`}
+                  type="text"
+                  name="relaventExperience"
+                  value={lineUpData.relaventExperience}
+                  onChange={handleChange}
+                  className="form-control"
+                 
                 />
                
               </td>
@@ -1076,6 +1213,7 @@ const CallingTrackerForm = ({
                       setLineUpData({
                         ...lineUpData,
                         finalStatus: e.target.value,
+                        required
                       })
                     }
                     className="form-select"
@@ -1121,3 +1259,10 @@ CallingTrackerForm.propTypes = {
 };
 
 export default CallingTrackerForm;
+
+
+
+
+
+
+
