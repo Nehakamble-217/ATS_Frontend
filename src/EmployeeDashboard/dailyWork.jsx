c0b36eb8ec1203a9e09a9b6656183a660ef0d1ca
rokadeswapnil <rokade65swapnil@gmail.com>
2024-06-28 16:03:29 +0530

    updated maindashboard

--- src/EmployeeDashboard/dailyWork.jsx
+++ src/EmployeeDashboard/dailyWork.jsx
@@ -56,7 +56,7 @@ function DailyWork({ successCount, successfulDataAdditions, archived, pending, h
     const fetchEmployeeData = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:8891/api/ats/157industries/employee-details/${employeeId}`
+          `http://192.168.1.39:8891/api/ats/157industries/employee-details/${employeeId}`
         );
         setEmployeeData(response.data);
         if (response.data.profileImage) {
@@ -284,7 +284,7 @@ function DailyWork({ successCount, successfulDataAdditions, archived, pending, h
 
 
      <div className="daily-timeanddate">
-            {/* <div className="header-clouds"></div> */}
+            <div className="header-clouds"></div>
 
 
       <div className="head">
