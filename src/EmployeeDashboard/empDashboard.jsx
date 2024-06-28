6ef76d6f8eaffb2819f07e2bf1e033bec28fd34e
rokadeswapnil <rokade65swapnil@gmail.com>
2024-06-28 10:12:51 +0530

    28/06/

--- src/EmployeeDashboard/empDashboard.jsx
+++ src/EmployeeDashboard/empDashboard.jsx
@@ -60,7 +60,7 @@ const EmpDashboard = ({ userGroup }) => {
   const [showEmployeeMasterSheet, setShowEmployeeMasterSheet] = useState(false);
   const [showShortListedCandidates, setShowShortListedCandidates] = useState(false);
   const [showUpdateCallingTracker, setShowUpdateCallingTracker] = useState(false);
-  const [showShortListedNav, setShowShortListdNav] = useState(true);
+  const [showShortListedNav, setShowShortListdNav] = useState(false);
   const [showAddEmployee,setShowAddEmployee] = useState(false)
   const [showNotePad,setShowNotePad] = useState(false)
   const [showProfile,setShowProfile] = useState(false)
@@ -289,15 +289,7 @@ const EmpDashboard = ({ userGroup }) => {
   }
 
   return (
-    <div className="w-full relative">
-      <div className="time-and-data">
-          <DailyWork 
-          employeeId={employeeId} 
-          successfulDataAdditions={successfulDataAdditions} 
-          profilePageLink={profilePageLink}
-          />
-        </div>
-        <div className={`grid-container ${openSidebarToggle ? 'sidebar-open' : 'sidebar-closed'}`}>
+    <div className={`grid-container ${openSidebarToggle ? 'sidebar-open' : 'sidebar-closed'}`}>
       <Sidebar 
         userGroup={userGroup}
         openSidebarToggle={openSidebarToggle}
@@ -328,7 +320,16 @@ const EmpDashboard = ({ userGroup }) => {
         toggleShareLink={toggleShareLink}
 
       />
-    <div className="empDash-main-content">
+        
+      <div className="empDash-main-content">
+        <div className="time-and-data">
+          <DailyWork 
+          employeeId={employeeId} 
+          successfulDataAdditions={successfulDataAdditions} 
+          profilePageLink={profilePageLink}
+          />
+        </div>
+
     <div>
       { showProfile && <EmployeeProfileData></EmployeeProfileData>}
     </div>
@@ -443,7 +444,6 @@ const EmpDashboard = ({ userGroup }) => {
         <div>
           {assignColumns && <Team_Leader/>}
         </div>
-      </div>
       </div>
     </div>
   );
